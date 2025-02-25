52739971b83ac6b37077a2007726a512cf681292
Pedro Antonny <123685133+PedroAntonny@users.noreply.github.com>
2025-02-25 15:52:16 -0300

    feat: add unique constraint to restaurant slug

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "u9a6wmr3as.ufs.sh",
+        pathname: "/f/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
